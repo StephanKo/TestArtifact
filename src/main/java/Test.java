1dfd4539a5ed354d7faf2a71f74b98649ca326ba
StephanKo <stdmko@gmail.com>
2019-10-24 15:16:01 +0300

    1commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,19 +1,39 @@
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class Test {
-   public static void main(String[]arg) {
-        open("http://google.com/ncr");
-        // $(By.name("q")).val("5").pressEnter();
 
-      /*  $$("#ires .g").shouldHave(size(10));
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
 
-        $("#ires .g").shouldBe(visible).shouldHave(
-                text("5: concise UI tests in Java"),
-                text("5.org"));
-                */
+//setting the driver executable
+        System.setProperty("webdriver.chrome.driver", "\\src\\test\\Other\\chromedriver.exe");
+
+//Initiating your chromedriver
+        WebDriver driver=new ChromeDriver();
+
+//Applied wait time
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//maximize window
+        driver.manage().window().maximize();
+
+//open browser with desried URL
+        driver.get("https://www.google.com");
+
+//closing the browser
+        driver.close();
+
     }
+
 }
